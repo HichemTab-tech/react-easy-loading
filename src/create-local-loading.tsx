046be2b3ad5ea01fc26c1ad96a1c6d465faea581
HichemTab-tech <konanhichemsinshi@gmail.com>
2026-01-02 16:46:46 +0100

    fix: prevent creation on every rerender

--- src/create-local-loading.tsx
+++ src/create-local-loading.tsx
@@ -1,14 +1,21 @@
 import {createLoading, type CreateLoadingOptions, type Loading} from "./create-loading";
 import {useEffect, useRef} from "react";
 
+const create = <Errors extends unknown[] = string[], Context extends Record<string, unknown> = any>(createLoadingOptions: CreateLoadingOptions|CreateLoadingOptions['initialState']) => createLoading<Errors, Context>(typeof createLoadingOptions === "string" ? {
+    initialState: createLoadingOptions
+} : createLoadingOptions);
+
 export const useCreateLocalLoading = <Errors extends unknown[] = string[], Context extends Record<string, unknown> = any>(createLoadingOptions: CreateLoadingOptions|CreateLoadingOptions['initialState'] = {}): Loading<Errors, Context> => {
-    const loading = useRef(createLoading<Errors, Context>(typeof createLoadingOptions === "string" ? {
-        initialState: createLoadingOptions
-    } : createLoadingOptions));
+    const loading = useRef<ReturnType<typeof create<Errors, Context>>>(undefined!);
+
+    if (!loading.current) {
+        loading.current = create<Errors, Context>(createLoadingOptions);
+    }
 
     useEffect(() => {
         return () => {
-            loading.current.destroy()
+            loading.current.destroy();
+            loading.current = undefined!;
         }
     }, []);
 
